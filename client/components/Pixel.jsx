940a1bab464efb9615092034cff85204431d102f
Jeremy Magill <jeremy.magill93@gmail.com>
2019-08-27 11:43:13 +1200

    added event handlers

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -8,19 +8,42 @@ class Pixel extends React.Component {
     this.state = { style: { backgroundColor: `${randomHexColor()}`, width: '50px', height: '50px' } }
   }
 
+  randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
   clickHandler = evt => {
     this.setState({
       style: {
-        backgroundColor: 'red',
+        backgroundColor: `${this.randomHexColor()}`,
         width: '100px',
         height: '100px'
       }
     })
   }
 
+  mouseEnterHandler = evt => {
+    this.setState({
+      style: {
+        backgroundColor: 'green',
+        width: '50px',
+        height: '50px'
+      }
+    })
+  }
+
+  contextMenu = evt => {
+    this.setState({
+      style: {
+        backgroundColor: 'black',
+        width: '20px',
+        height: '20px'
+      }
+    })
+  }
+
   render () {
     return (
-      <div style={this.state.style} onClick={this.clickHandler}> </div>
+      <div style={this.state.style} onClick={this.clickHandler} onMouseEnter={this.mouseEnterHandler} onContextMenu={this.contextMenu}> </div>
     )
   }
 }
