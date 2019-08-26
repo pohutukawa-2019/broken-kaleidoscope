f9269d4c43835ff8fedf44d047e2a35c4a1aef84
Jeremy Magill <jeremy.magill93@gmail.com>
2019-08-27 11:26:03 +1200

    randomHexColor working.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,69 @@ import Pixel from './Pixel'
 const App = () => {
   return (
     <React.Fragment>
-      <div>React development has begun!</div>
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
       <Pixel />
     </React.Fragment>
   )
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style={{ backgroundColor: 'blue', width: '50px', height: '50px' }}> </div>
-  )
+class Pixel extends React.Component {
+  constructor (props) {
+    super(props)
+    const randomHexColor = () =>
+      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+    this.state = { style: { backgroundColor: `${randomHexColor()}`, width: '50px', height: '50px' } }
+  }
+
+  render () {
+    return (
+      <div style={this.state.style}> </div>
+    )
+  }
 }
+
+// const Pixel = () => {
+//   return (
+//     <div style={{ backgroundColor: 'blue', width: '50px', height: '50px' }}> </div>
+//   )
+// }
 
 export default Pixel
