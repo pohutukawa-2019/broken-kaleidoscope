2b2e1ebb6518fa8b97b1f932ac7822da8f9a8cba
Michael Hodges <michaelcshodges@gmail.com>
2019-08-27 15:06:00 +1200

    add events

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { genericTypeAnnotation } from '@babel/types';
 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
@@ -13,11 +14,71 @@ class Pixel extends React.Component {
     }
   }
 
-render () {
-  return (
-    <div style = {this.state.style}></div>
-  )
-}
+  clickHandler = evt => {
+    this.setState({
+      style: {
+        backgroundColor: randomHexColor(),
+        height: "50px",
+        width: "50px"
+      }
+    })
+  }
+
+  mouseOverHandler = evt => {
+    this.setState({
+      style: {
+        backgroundColor: "green",
+        height: "50px",
+        width: "50px"
+      }
+    })
+  }
+
+  contextMenuHandler = evt => {
+    evt.preventDefault()
+    this.setState({
+      style: {
+        backgroundColor: "black",
+        height: "50px",
+        width: "50px"
+      }
+    })
+  }
+
+  doubleClickHandler = evt => {
+    this.setState({
+      style: {
+        backgroundColor: "white",
+        height: "50px",
+        width: "50px"
+      }
+    })
+  }
+
+  dragEnterHandler = evt => {
+    this.setState({
+      style: {
+        backgroundColor: "yellow",
+        height: "50px",
+        width: "50px"
+      }
+    })
+  }
+
+
+  render () {
+    return (
+      <div 
+        style = {this.state.style} 
+        onClick = {this.clickHandler} 
+        onMouseEnter = {this.mouseOverHandler} 
+        onContextMenu = {this.contextMenuHandler} 
+        onDoubleClick = {this.doubleClickHandler}
+        onDragEnter = {this.dragEnterHandler}
+      >
+      </div>
+    )
+  }
 
 }
 
