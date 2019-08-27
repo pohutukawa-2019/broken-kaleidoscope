e73d00e61b59c41c3283df2469f0b07c0c410666
Sam Bowering <samjbowering@gmail.com>
2019-08-27 15:23:38 +1200

    Onclick random colour change added

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,17 +1,30 @@
 import React from 'React'
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
 class Pixel extends React.Component {
-  state = {style: { 
-    width: "20px",
-    height: "20px",  
-    backgroundColor: "blue" 
-    }
+  clickHandler = () => {
+    this.setState({
+      style: {
+        width: "20px",
+        height: "20px",
+        backgroundColor: randomHexColor()
+      }
+    })
   }
   
+  state = {style: { 
+    width: "20px",
+    height: "20px",
+    backgroundColor: randomHexColor()
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div style={this.state.style}></div>
+        <div style={this.state.style} onClick={this.clickHandler}></div>
       </React.Fragment>
     )
   }
